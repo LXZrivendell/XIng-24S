9859c82c26d7acab075cf2338a321e68677a31ea
LXZrivendell <ntlxz1001@163.com>
2025-04-20 19:41:56 +0800

    solve .gitignore nothing done with os

---
+++ ci-user/check/ch6.py
@@ -0,0 +1,22 @@
+import base
+from ch6b import EXPECTED_6b
+from ch5 import EXPECTED_5, NOT_EXPECTED_4b
+
+EXPECTED_6 = list(set(EXPECTED_5 + EXPECTED_6b + [
+    # ch6_file0
+    "Test file0 OK11958!",
+
+    # ch6_file1
+    "Test fstat OK11958!",
+
+    # ch6_file2
+    "Test link OK11958!",
+
+    # ch6_file3
+    "Test mass open/unlink OK11958!",
+]))
+
+EXPECTED_6 = list(set(EXPECTED_6) - set(["Test set_priority OK11958!"]))
+
+if __name__ == "__main__":
+    base.test(EXPECTED_6, NOT_EXPECTED_4b)
